9a2df687f8787b951365484454ddae9b6267e265
Muhammad Sohail <msohail.se@gmail.com>
2022-05-23 22:24:02 +0500

    Update employee.js

--- models/employee.js
+++ models/employee.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 // Define collection and schema
-let Employee = new Schema({
+const Employee = new Schema({
    name: {
       type: String
    },
